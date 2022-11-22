9aae105851512f4bdd347a0c7dfd62700168ab3a
AmpereurZeg <abdoulrazak.amperouzeguime@gmail.com>
2022-11-22 06:09:22 +0100

    acquisitionModified

--- exercice-1/acquisitionManagerPOSIX.c
+++ exercice-1/acquisitionManagerPOSIX.c
@@ -12,21 +12,27 @@
 #include "debug.h"
 
 #include <stdatomic.h> // for atomic variable 
-#define SEM_NAME "/acquisitionManager_sem"
-#define SEMAPHORE_INITIAL_VALUE 0
-volatile MSG_BLOCK msg_bloc; // define a message 
+#define SEM_NAME "/sem_libre"
+#define BUFF_SIZE 4 // total number of input 
+#define SL_INITIAL_VALUE 4
+
+
+volatile MSG_BLOCK msg_bloc[BUFF_SIZE]; // shared message bloc
+int indiceLibre;       // indicate the empty input index
+int indiceOccupee;     // indicate the nomber of fill input index
 //producer count storage
 _Atomic int produceCount = 0; // replace volatile with atomic for more thread safe operation
 
 pthread_t producers[4];
-
 static void *produce(void *params);
 
 /**
 * Semaphores and Mutex
 */
 //TODO
-sem_t *semAcquisition ;
+sem_t  *SL;  // number of empty spot
+sem_t *SCOM1; // getting the empty index
+sem_t *SCOM2;  // giving the fill index
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 /*
 * Creates the synchronization elements.
@@ -44,8 +50,10 @@ static unsigned int createSynchronizationObjects(void)
 
 	//TODO
 	sem_unlink(SEM_NAME);
-    semAcquisition = sem_open(SEM_NAME, O_CREAT, 0644, SEMAPHORE_INITIAL_VALUE);
-	if (semAcquisition == SEM_FAILED){
+    SL = sem_open(SEM_NAME, O_CREAT, 0644, SL_INITIAL_VALUE);
+	SCOM1 = sem_open("/sem_com1", O_CREAT, 0644, 1);
+	SCOM2 = sem_open("/sem_com2", O_CREAT, 0644, 1);
+	if (SL == SEM_FAILED){
         perror("sem_open");
         return ERROR_INIT;
     }
@@ -69,7 +77,7 @@ unsigned int getProducedCount(void)
 
 MSG_BLOCK getMessage(void){
 	//TODO
-	return msg_bloc;
+	return msg_bloc[indiceOccupee];
 }
 
 //TODO create accessors to limit semaphore and mutex usage outside of this C module.
@@ -107,7 +115,9 @@ void acquisitionManagerJoin(void)
 
 	//TODO
 	printf("Deleting the semaphore\n");
-    sem_destroy(semAcquisition);
+	sem_destroy(SCOM1);
+	sem_destroy(SCOM2);
+    sem_destroy(SL);
 	printf("[acquisitionManager]Semaphore cleaned\n");
 }
 
@@ -115,19 +125,35 @@ void *produce(void* params)
 {
 	D(printf("[acquisitionManager]Producer created with id %d\n", gettid()));
 	unsigned int i = 0;
+	int currentIndex = 0; // local input index
+	int j = 0; // compteur for number of input
+	MSG_BLOCK currentMsg; // message provided
 	while (i < PRODUCER_LOOP_LIMIT)
 	{
 		i++;
 		sleep(PRODUCER_SLEEP_TIME+(rand() % 5));
 		//TODO
+		getInput(gettid(), &currentMsg);
+		if(!messageCheck(&currentMsg)){
+			continue;
+		}
+		sem_wait(SL);
+		sem_wait(SCOM1);
+		currentIndex = indiceLibre;
+		indiceLibre = (indiceLibre+1)%BUFF_SIZE;
+		sem_post(SCOM1);
+		msg_bloc[currentIndex] = currentMsg;
 		printf("Trying to own the mutex\n");
     	pthread_mutex_lock(&mutex);
     	printf("Owns the mutex\n");
-		getInput(gettid(), &msg_bloc); // get input message
 		incrementProducedCount(); //incremente le nombre d'entree produit 
-		break; // exit the loop when we get the message
+		pthread_mutex_unlock(&mutex);
+		sem_wait(SCOM2);
+		indiceOccupee=currentIndex;
+		indiceOccupee = (indiceOccupee+1)%BUFF_SIZE;
+		sem_post(SCOM2);
 	}
 	printf("[acquisitionManager] %d termination\n", gettid());
 	//TODO
-	pthread_mutex_unlock(&mutex);
+	sem_post(SL);
 }
